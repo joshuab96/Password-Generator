43990699b373916709a914c9ce906d6853f2d98c
Joshua-Rhymer Bernardo <joshua.bernardo@live.com>
2021-03-17 13:14:21 +1100

    Addded updated and added window prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,29 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
+function UserPassword() {
+  window.prompt("How many characters long would you like you password to be? must be between 8 and 128, please input number") 
+    
+    if (UserPassword < 8 && UserPassword > 128 )
+    
+    window.prompt("Must only be between 8 and 128 characters long, please try again.");
+    
+    else if (UserPassword > 8 && UserPassword < 128 )
+    
+    window.prompt("joidjewijdio ");
+
+  };
+
+  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-}
+
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", UserPassword); {
+  
+}
