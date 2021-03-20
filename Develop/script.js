ffaf0d12daf8fc568ac652903f7f640a4ca5254b
Joshua-Rhymer Bernardo <joshua.bernardo@live.com>
2021-03-20 21:15:13 +1100

    restructured js doc

--- Develop/script.js
+++ Develop/script.js
@@ -2,41 +2,67 @@
 
 
 
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCaseCHharacters = "abcdefghijklmnopqrstuvwxyz";
 
+var upperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-function generatePassword() {
-  
-  var length = window.prompt("How long do you want the password to be?");
- 
-  while (length < 8 || length > 128)
-
-   length = window.prompt("Password has to be between 8 and 128 characters long, please enter a new length"); //getting password length from the user
-
-    length = parseInt(length);
-
-   var upperCase = window.confirm("Do you want uppercase characters?"); //confirming Upercase characters from user
-  
-  if (upperCase = true);
-
-  console.log(upperCase);
-}
-
-  
-  
-
+var specialCharacters = "!@#$%^&*()";
 
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//Requests password length input via window prompt.
+var passwordLength = window.prompt("To generate password, please enter password length between 8 and 128"); 
 
+//if user presses cancel, the functions ends immediately.
+if (!passwordLength) { 
+  return;
 }
+
+//Determines whether length input number is between 8 and 128
+if (parseInt(passwordLength) >= 8 && parseInt(passwordLength) <= 128) {
+  console.log(passwordLength); 
+
+//will alert user if password length chosen is not between 8 and 128  
+}else window.confirm("Error, please choose a password length between 8 and 128 and try again")
+}
+
+
+// var password = generatePassword();
+var passwordText = document.querySelector("#password");
+
+passwordText.value = password;
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
+
+
+
+
+// -------------------------------------------------
+
+  // var length = window.prompt("How long do you want the password to be? Please input number between 8 and 128");//getting password length from the user
+ 
+  // if (parseInt(length) >= 8 && parseInt(length) <= 128)//converting user length input into number value
+
+  // console.log(length);
+
+  // // else window.confirm("Password can only be between 8 and 128, please enter a valid number and try again."); //if user enter a number that not between 8 and 128
+
+  // // window.confirm("Do you want uppercase characters?"); //confirming Upercase characters from user
+  
+  // // if (upperCase = true);
+
+  // // console.log(upperCase);
+
